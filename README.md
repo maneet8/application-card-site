4f3991d46ab27c92d4e40ab794a7be3b0aa2eac4
Maneet Kumar <maneetkum@gmail.com>
2017-11-05 20:44:25 +0530

    Basic Structure for the Feedback Application.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # application-card-site
+
+This Application will be helpful for taking Feedback about the sessions in Technology Conclave.
+
+- This Application is based on Angular JS.
+- In this Bootstrap Card and Modal are Used.
---
+++ src/assets/js/mainController.js
@@ -0,0 +1,75 @@
+var app = angular.module("myApp", ["ngRoute"]);
+app.controller("myCtrl", function ($scope) {
+    $scope.lhn = [
+        {
+            "name": "Maneet Kumar",
+            "imgpath": "./assets/img/angularjslogo.jpg",
+            "topic": "Angular JS",
+            "Description": "",
+            "id": "angularjs",
+            "url": "#!angularjs"
+        }, {
+            "name": "Aditya",
+            "imgpath": "./assets/img/awslogologo.jpg",
+            "topic": "AWS",
+            "Description": "",
+            "id": "aws",
+            "url": "#!aws"
+        }, {
+            "name": "Arun",
+            "imgpath": "./assets/img/bootstraplogo.jpg",
+            "topic": "Bootstrap",
+            "Description": "",
+            "id": "bootstrap",
+            "url": "#!bootstrap"
+        }, {
+            "name": "Sarang Bapat",
+            "imgpath": "./assets/img/restfullogo.jpg",
+            "topic": "Restful",
+            "Description": "",
+            "id": "restful",
+            "url": "#!restful"
+        },
+        {
+            "name": "Renchu K",
+            "imgpath": "./assets/img/html5logo.jpg",
+            "topic": "HTML5",
+            "Description": "",
+            "id": "html5",
+            "url": "#!html5"
+        },
+        {
+            "name": "Shyamal",
+            "imgpath": "./assets/img/angularmateriallogo.jpg",
+            "topic": "Angular Material",
+            "Description": "",
+            "id": "angularmaterial",
+            "url": "#!angularmaterial"
+        },
+        {
+            "name": "Rishi",
+            "imgpath": "./assets/img/firebaselogo.jpg",
+            "topic": "Firebase",
+            "Description": "",
+            "id": "firebase",
+            "url": "#!firebase"
+        },
+        {
+            "name": "Ashish",
+            "imgpath": "./assets/img/javascriptlogo.jpg",
+            "topic": "Javascript",
+            "Description": "",
+            "id": "javascript",
+            "url": "#!javascript"
+        },
+        {
+            "name": "Sudarshika",
+            "imgpath": "./assets/img/jqueryuilogo.jpg",
+            "topic": "JQuery UI",
+            "Description": "",
+            "id": "jqueryui",
+            "url": "#!jqueryui"
+        }
+    ]
+
+});
