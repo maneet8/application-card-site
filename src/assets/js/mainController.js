721e65a1acd79eff4a636855e6495a76f36060a3
Kumar (EXT) <rm9wnk@splm.siemens.com>
2017-11-06 19:14:12 +0530

    For Including iframe in feedback modal

--- src/assets/js/mainController.js
+++ src/assets/js/mainController.js
@@ -1,75 +1,84 @@
 var app = angular.module("myApp", ["ngRoute"]);
-app.controller("myCtrl", function ($scope) {
+app.controller("myCtrl", function ($scope, $sce) {
     $scope.lhn = [
         {
             "name": "Maneet Kumar",
             "imgpath": "./assets/img/angularjslogo.jpg",
             "topic": "Angular JS",
             "Description": "",
-            "id": "angularjs",
-            "url": "#!angularjs"
-        }, {
+            "id": "1",
+            "url": "https://docs.google.com/forms/d/e/1FAIpQLSdEfI7ILuKb-DoTGfbYyc5oO9Lew0q-6ZL82TpT2oSYZH1eYw/viewform?embedded=true"
+        },
+        {
             "name": "Aditya",
             "imgpath": "./assets/img/awslogologo.jpg",
             "topic": "AWS",
             "Description": "",
-            "id": "aws",
-            "url": "#!aws"
-        }, {
+            "id": "2",
+            "url": "http://localhost:9092/"
+        },
+        {
             "name": "Arun",
             "imgpath": "./assets/img/bootstraplogo.jpg",
             "topic": "Bootstrap",
             "Description": "",
-            "id": "bootstrap",
-            "url": "#!bootstrap"
-        }, {
+            "id": "3",
+            "url": "http://localhost:8001/"
+        },
+        {
             "name": "Sarang Bapat",
             "imgpath": "./assets/img/restfullogo.jpg",
             "topic": "Restful",
             "Description": "",
-            "id": "restful",
-            "url": "#!restful"
+            "id": "4",
+            "url": "http://localhost:8001/"
         },
         {
             "name": "Renchu K",
             "imgpath": "./assets/img/html5logo.jpg",
             "topic": "HTML5",
             "Description": "",
-            "id": "html5",
-            "url": "#!html5"
+            "id": "5",
+            "url": "http://localhost:8001/"
         },
         {
             "name": "Shyamal",
             "imgpath": "./assets/img/angularmateriallogo.jpg",
             "topic": "Angular Material",
             "Description": "",
-            "id": "angularmaterial",
-            "url": "#!angularmaterial"
+            "id": "6",
+            "url": "http://localhost:8001/"
         },
         {
             "name": "Rishi",
             "imgpath": "./assets/img/firebaselogo.jpg",
             "topic": "Firebase",
             "Description": "",
-            "id": "firebase",
-            "url": "#!firebase"
+            "id": "7",
+            "url": "http://localhost:8001/"
         },
         {
             "name": "Ashish",
             "imgpath": "./assets/img/javascriptlogo.jpg",
             "topic": "Javascript",
             "Description": "",
-            "id": "javascript",
-            "url": "#!javascript"
+            "id": "8",
+            "url": "http://localhost:8001/"
         },
         {
             "name": "Sudarshika",
             "imgpath": "./assets/img/jqueryuilogo.jpg",
             "topic": "JQuery UI",
             "Description": "",
-            "id": "jqueryui",
-            "url": "#!jqueryui"
+            "id": "9",
+            "url": "https://docs.google.com/forms/d/e/1FAIpQLSdEfI7ILuKb-DoTGfbYyc5oO9Lew0q-6ZL82TpT2oSYZH1eYw/viewform?embedded=true"
         }
     ]
+
+    $scope.setProject = function (id) {
+        $scope.currentProject = $scope.lhn[id];
+        $scope.currentProjectUrl = $sce.trustAsResourceUrl($scope.currentProject.url);
+    }
+}
 
 });
